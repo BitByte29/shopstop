f574c2b2de2578f86ac5be8ad99c1d3272e7a5ea
bitbyte29 <4gaminghsp@gmail.com>
2023-11-01 14:39:15 +0530

    host error fixed

--- src/App/features/host.js
+++ src/App/features/host.js
@@ -1,5 +1,5 @@
 export const getserver = () => {
-  // const server = "https://shopstopAPI.onrender.com";
-  const server = "http://localhost:3001";
+  const server = "https://shopstopAPI.onrender.com";
+  // const server = "http://localhost:3001";
   return server;
 };
