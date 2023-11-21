5664d6adeab9ffd43d02e0963a044b2ab9a2e300
bitbyte29 <4gaminghsp@gmail.com>
2023-11-21 10:28:33 +0530

    some style changes

--- src/components/Home/FeaturedProducts.js
+++ src/components/Home/FeaturedProducts.js
@@ -4,9 +4,14 @@ import Product from "../Products/Product";
 const FeaturedProducts = ({ products }) => {
   return (
     <div className="w-full md:px-[150px] px-4 bg-lp pb-16">
-      <h2 className="py-10 text-3xl text-center">
-        <span className="px-3 py-4 font-semibold bg-yellow-300">
+      <h2 className="py-10 text-3xl flex items-center justify-center h-auto">
+        {/* <span className="md:px-3 md:py-4 px-2 md:font-semibold bg-yellow-300 h-full">
           Featured Products
+        </span> */}
+        <span className="flex-center max-w-full md:px-3 font-semibold md:py-4 px-2 sm:flex-row sm:gap-2 bg-yellow-300 h-full ">
+          <span className="pt-2 sm:pt-0">Featured</span>
+          <span className="pb-2 sm:pb-0">Products</span>
+          {/* Featured Products */}
         </span>
       </h2>
       <div className="flex flex-wrap items-center justify-center gap-6 ">
